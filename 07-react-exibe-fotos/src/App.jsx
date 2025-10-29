aa477e7d5e552b2b5ae58fe5603c4793f012e7cb
felip-000 <felipecostamuniz@gmail.com>
2025-10-29 11:04:40 -0300

    learn(ap07-react): pencerra secao 2.6 - retomar fazendo as requisições, lembrar de isolar a chave no backend no futuro

--- 07-react-exibe-fotos/src/App.jsx
+++ 07-react-exibe-fotos/src/App.jsx
@@ -1,17 +1,24 @@
 import React from "react"
 import Busca from "./Busca"
 
-const App = () => {
-  return (
-    <div className="grid border-1 border-400 border-round-sm p-3 mx-3 mt-3">
-      <div className="col-12">
-        <i className="pi pi-truck"></i>
+class App extends React.Component {
+
+  onBuscaRealizada = (termoDeBusca) => {
+    const chaveAPI = 'T7bwNCRAKRad5zIYpu0Q7RsVvPdwFVMNpteLwLHz52GxWF9pd763jrVT'
+    console.log(termoDeBusca)
+  }
+  render(){
+    return (
+      <div className="grid border-1 border-400 border-round-sm p-3 mx-3 mt-3">
+        <div className="col-12">
+          <i className="pi pi-truck"></i>
+        </div>
+        <div className="col-12">
+          <Busca onBuscaRealizada={this.onBuscaRealizada}/>
+        </div>
       </div>
-      <div className="col-12">
-        <Busca/>
-      </div>
-    </div>
-  )
+    )
+  }  
 }
 
 export default App
